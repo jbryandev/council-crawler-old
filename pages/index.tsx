41b43b838fdc09c708093b312e73eb67504ec14c
jbryan83 <james.bryan.home@gmail.com>
2022-10-28 12:50:47 -0500

    Post-codemod run

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,32 +12,32 @@ type Props = {
 };
 
 export default function Index({ agencies }: Props) {
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>Council Crawler</title>
-        </Head>
-        <Container>
-          <Intro />
-          <section className='flex-col md:flex-row flex items-center md:justify-between'>
-            <ul>
-              {agencies &&
-                agencies.map((agency) => (
-                  <li className='list-disc list-inside' key={agency.id}>
-                    <Link href={`/${encodeURIComponent(agency.slug)}`}>
-                      <a className='underline hover:text-success duration-200 transition-colors'>
-                        {agency.name}
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-            </ul>
-          </section>
-        </Container>
-      </Layout>
-    </>
-  );
+  return <>
+    <Layout>
+      <Head>
+        <title>Council Crawler</title>
+      </Head>
+      <Container>
+        <Intro />
+        <section className='flex-col md:flex-row flex items-center md:justify-between'>
+          <ul>
+            {agencies &&
+              agencies.map((agency) => (
+                <li className='list-disc list-inside' key={agency.id}>
+                  <Link
+                    href={`/${encodeURIComponent(agency.slug)}`}
+                    className='underline hover:text-success duration-200 transition-colors'>
+
+                    {agency.name}
+
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </section>
+      </Container>
+    </Layout>
+  </>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
